1c5d522a1320cf4ec8fc3577194d6eee9619f23a
tomitahibiki <n4f0s8jmht9xkbxn2mhz@docomo.ne.jp>
2017-03-31 17:39:20 +0900

    GoForwaerUnityChan2

---
+++ Assets/CubeController.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class CubeController : MonoBehaviour
+{
+    public AudioClip block;
+
+
+    // キューブの移動速度
+    private float speed = -0.2f;
+
+    // 消滅位置
+    private float deadLine = -10;
+
+    // Use this for initialization
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+
+        // キューブを移動させる
+        transform.Translate(this.speed, 0, 0);
+
+        // 画面外に出たら破棄する
+        if (transform.position.x < this.deadLine)
+        {
+            Destroy(gameObject);
+
+
+
+        }
+
+
+    }
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        AudioSource audioSource = gameObject.GetComponent<AudioSource>();
+        audioSource.clip = block;
+        audioSource.Play();
+
+        if (collision.gameObject.tag == "UnityChanTag")
+
+
+            GetComponent<AudioSource>().volume = 0;
+    }
+}
